1ae84a43498b16af0ecfc35c17f4e1da347ebd82
nirojneupane5 <nirojneupane5@gmail.com>
2024-05-22 13:15:06 +0545

    Added .env file

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const mongoURI="mongodb://localhost:27017";
+dotenv.config();
+
+const mongoURI=process.env.MONGOURI!;
 //database connection
 export const connectDB=()=>{
     try{
