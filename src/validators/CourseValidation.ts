b30a7bec752861be91bd0116dad81b99c8f32b56
nirojneupane5 <nirojneupane5@gmail.com>
2024-05-26 16:11:47 +0545

    Course description added

--- src/validators/CourseValidation.ts
+++ src/validators/CourseValidation.ts
@@ -5,6 +5,10 @@ export const createCourseSchema=Joi.object({
         "string.base":"Please enter the valid course",
         "any.required":"Course is required"
     }),
+    course_desc:Joi.string().required().messages({
+        "string.base":"Please enter the valid course description",
+        "any.required":"Course is required"
+    }),
     price:Joi.number().greater(0).required().messages({
         "number.base":"Price must be number",
         "number.greater":"Price must be greater than 0",
